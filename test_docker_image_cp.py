2e135d0d3bb4d1b9bb19d24a6acf0289424d02e9
Tim Vergenz <vergenzt@gmail.com>
2024-06-20 12:25:02 -0400

    update tests

--- test_docker_image_cp.py
+++ test_docker_image_cp.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import subprocess as sp
 import sys
 import tempfile
+from typing import List
 import unittest
 
 import docker_image_cp
@@ -16,6 +17,10 @@ def docker_dir(src: str, cnt: str):
             "FROM scratch\n" f"COPY {src} ./\n" "CMD sh\n"
         )
         yield tmp
+
+
+def docker_ids(cmd: List[str]):
+    return set(sp.check_output(cmd, text=True).splitlines())
 
 
 def run_py(*args: str):
@@ -32,6 +37,29 @@ class Tests(unittest.TestCase):
             dst = stack.enter_context(tempfile.TemporaryDirectory())
             run_py("-b", ctx, src, f"{dst}/{src}")
 
-    def test_no_image(self):
+    def test_image_arg(self):
+        with ExitStack() as stack:
+            src = "preexisting.txt"
+            cnt = "hello, world!"
+            ctx = stack.enter_context(docker_dir(src, cnt))
+            img = sp.check_output(["docker", "build", "-q", ctx], text=True).strip()
+            dst = stack.enter_context(tempfile.TemporaryDirectory())
+            run_py("-i", img, src, f"{dst}/{src}")
+
+    def test_no_cleanup_arg_imgs(self):
+        imgs_cmd = ["docker", "images", "-q"]
+        imgs_before = docker_ids(imgs_cmd)
+        self.test_build_arg()
+        imgs_after = docker_ids(imgs_cmd)
+        self.assertSetEqual(imgs_before, imgs_after)
+
+    def test_no_cleanup_arg_cnts(self):
+        cnts_cmd = ["docker", "ps", "-q"]
+        cnts_before = docker_ids(cnts_cmd)
+        self.test_build_arg()
+        cnts_after = docker_ids(cnts_cmd)
+        self.assertSetEqual(cnts_before, cnts_after)
+
+    def test_no_image_arg(self):
         with self.assertRaises(sp.CalledProcessError):
             run_py("foo", "bar")
